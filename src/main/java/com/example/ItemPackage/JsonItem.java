744c3e6eb73cf59a8f9752cf39ebce97b031a541
Sangfer <alexis.barthelemy02@gmail.com>
2016-11-29 18:25:25 +0100

    Method for creating an Item from a Json

--- src/main/java/com/example/ItemPackage/JsonItem.java
+++ src/main/java/com/example/ItemPackage/JsonItem.java
@@ -5,6 +5,9 @@
  */
 package com.example.ItemPackage;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -15,21 +18,104 @@ import org.json.JSONObject;
 public class JsonItem {
     
     private JSONObject jsonItem;
+    private Item myItem;
     
     public JsonItem(){
     }
+    
+    
 
     public JSONObject getJsonItem() {
         return jsonItem;
     }
 
+    public JSONObject getJsonItem(int item) throws Exception{
+        this.getItem(item);
+        return getJsonItem();  
+    }
+    
+
     public void setJsonItem(JSONObject jsonItem) {
         this.jsonItem = jsonItem;
     }
         
-    public void GetItem(int item) throws Exception {
-    HttpURLConnection http = new HttpURLConnection();
-    String stringItem= http.getItemByNumber(item);
-    this.setJsonItem(new JSONObject(stringItem));
+    public void getItem(int item) throws Exception {
+        HttpURLConnection http = new HttpURLConnection();
+        String stringItem= http.getItemByNumber(item);
+        this.setJsonItem(new JSONObject(stringItem));
     }
+    
+    public void createItem(){
+        Item myItem= new Item(  getId(), getDescription(), getName(), getIcon(), 
+                                getBonusStats(), getItemClass(), getDammageMin(),
+                                getDammageMax(),getExactMin(), getExactMax(), 
+                                getWeaponSpeed(), getDps(), getBaseArmor(),
+                                isHasSockets());    
+    }
+    
+    public int getId() {
+        return jsonItem.getInt("id");
+    }
+     
+    public String getDescription() {
+        return jsonItem.getString("description");
+    }
+ 
+    public String getName() {
+        return jsonItem.getString("name");
+    }
+       
+    public String getIcon() {
+        return jsonItem.getString("icon");
+    }
+       
+    public List<Stat> getBonusStats() {
+        List<Stat> myList = new ArrayList<>();
+        JSONArray tmp = jsonItem.getJSONArray("bonusStats"); 
+        for(Object o: tmp){
+            JSONObject jsonlineItem=(JSONObject) o;
+            int statNumber=jsonlineItem.getInt("stat");
+            int amount=jsonlineItem.getInt("amount");
+            Stat tmpStat= new Stat(statNumber, amount);
+            myList.add(tmpStat);
+        }
+        return myList;
+    }
+       
+    public int getItemClass() {
+        return jsonItem.getInt("ItemClass");
+    }
+        
+    public int getDammageMin() {
+        return jsonItem.getInt("min");
+    }
+    
+    public int getDammageMax() {
+        return jsonItem.getInt("max");
+    }
+    
+    public int getExactMin() {
+        return jsonItem.getInt("exactMin");
+    }
+      
+    public int getExactMax() {
+        return jsonItem.getInt("exactMax");
+    }
+      
+    public int getWeaponSpeed() {
+        return jsonItem.getInt("weaponSeed");
+    }
+      
+    public int getDps() {
+        return jsonItem.getInt("dps");
+    }
+    
+    public int getBaseArmor() {
+        return jsonItem.getInt("baseArmor");
+    }
+        
+    public boolean isHasSockets() {
+        return jsonItem.getBoolean("hasSockets");
+    }
+
 }
