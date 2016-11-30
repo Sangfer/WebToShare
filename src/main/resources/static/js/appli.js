00273e0fc289696ad514ecd03a61d0c551a803e9
Sangfer <alexis.barthelemy02@gmail.com>
2016-11-30 23:50:08 +0100

    non functionnal react part

--- src/main/resources/static/js/appli.js
+++ src/main/resources/static/js/appli.js
@@ -1,24 +1,63 @@
-class VegeApp extends React.Component{
-    
-    constructor(props){
-        super(props);
-            this.state= {veges: [
-                    {name: "Tomato", color: "red", price: "10"},
-                    {name: "Cucumber", color:"green", price: "5"},
-            ]};
-        
-    }
-    
-    
-    render(){
-        return (
-                <h1> {this.props.titre} <b>toto</b></h1>
-                )
-    }
+const React = require('react');
+const ReactDOM = require('react-dom')
+const client = require('./client');
+
+
+class ItemApp extends React.Component{
+constructor(props) {
+		super(props);
+		this.state = {items: []};
+	}
+
+	componentDidMount() {
+		client({method: 'GET', path: '/api/items'}).done(response => {
+			this.setState({items: response.entity._embedded.items});
+		});
+	}
+
+	render() {
+		return (
+			<ItemList item={this.state.items}/>
+		)
+	}
 }
 
+class ItemList extends React.Component{
+	render() {
+		var items = this.props.items.map(item =>
+			<Item key={item._links.self.href} item={item}/>
+		);
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<th>id</th>
+						<th>name</th>
+						<th>bonusStat</th>
+                                                <th>itemClass</th>
+					</tr>
+					{items}
+				</tbody>
+			</table>
+		)
+	}
+}
 
-ReactDom.render(
-        <VegeApp> Hello <em>hardcoded</em></VegeApp>,
-        document.getElementById('container')        
-        );
+class Item extends React.Component{
+	render() {
+		return (
+			<tr>
+                                <td>{this.props.item.id}</td>
+                        	<td>{this.props.item.name}</td>
+				<td>{this.props.item.bonusStat}</td>
+				<td>{this.props.item.itemClass}</td>
+			</tr>
+		)
+	}
+}
+
+ReactDOM.render(
+	<ItemApp />,
+	document.getElementById('react')
+)
+
